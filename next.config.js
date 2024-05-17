449835f29d877d47a65ccea0e7b40cfa7afca4d0
muyoungko <muyoungko@11stcorp.com>
2024-05-17 15:13:04 +0900

    a

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  i18n: {
+    locales: ['en-US', 'ko-KR'],
+    defaultLocale: 'en-US',
+  },
+  darkMode: false,
 })
 
 module.exports = withNextra()
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,43 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Devil App Builder</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/muyoungko/dab-document',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/muyoungko/dab-document',
   footer: {
-    text: 'Nextra Docs Template',
+    text: <span>© 2024 Mondayless, Inc</span>,
+  },
+  darkMode:true,
+  useNextSeoProps() {
+    const { asPath } = useRouter()
+    if (asPath === '/') {
+      return {
+        titleTemplate: 'Documentation - Devil App Builder'
+      }
+    }
+  },
+
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const url =
+      'https://document.devil-app-builder.com' + (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+    
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={'Documentation - Devil App Builder'} />
+        <meta
+          property="og:description"
+          content={'The next site builder'}
+        />
+      </>
+    )
   },
 }
 
 export default config
+
